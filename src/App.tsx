bcbb4c0b54a5963e0bb4be89cc4706bddf04085d
Egor Yupatov <95219188+egoryupatov@users.noreply.github.com>
2022-10-24 13:58:40 +0600

    bug fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
+import { WeekScreen } from "./screens/WeekScreen/WeekScreen";
 import { useAppSelector } from "./store/hooks";
 import { selectCurrentDate } from "./store/dutiesSlice";
 import { PrivateRoutesGroup } from "./components/PrivateRoutesGroup";
@@ -11,7 +12,7 @@ function App() {
   return (
     <Routes>
       <Route path="/month/*" element={<PrivateRoutesGroup />} />
-      <Route path="/week/*" element={<PrivateRoutesGroup />} />
+      <Route path="/week/:year/:month/:day" element={<WeekScreen />} />
       <Route path="*" element={<Navigate to={url} />} />
     </Routes>
   );
--- src/components/PrivateRoutesGroup.tsx
+++ src/components/PrivateRoutesGroup.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { MonthScreen } from "../screens/MonthScreen/MonthScreen";
 import { LoginPageContainer } from "./LoginPage/LoginPageContainer";
-import { WeekScreen } from "../screens/WeekScreen/WeekScreen";
 
 export const PrivateRoutesGroup = () => {
   const isUserAuthorized = localStorage.getItem("token");
@@ -14,7 +13,6 @@ export const PrivateRoutesGroup = () => {
   return (
     <Routes>
       <Route path=":year/:month/:day" element={<MonthScreen />} />
-      <Route path=":year/:month/:day" element={<WeekScreen />} />
     </Routes>
   );
 };
